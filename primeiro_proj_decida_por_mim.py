ad364bc6bd953fd05be47c3591b7ac200df42607
Fernando Pinho <39680626+fa2pinho@users.noreply.github.com>
2020-06-23 15:09:39 +0100

    Update primeiro_proj_decida_por_mim.py

--- primeiro_proj_decida_por_mim.py
+++ primeiro_proj_decida_por_mim.py
@@ -1,6 +1,10 @@
 #Decida por mim
 '''Objetivos: 
-Seu programa deve permitir que o usuário faça uma pergunta para o programa, seu programa deve exibir na tela que está pensando(isso deve tomar alguns segundos) e depois o programa deve responder o usuário, dentro de 10 possibilidades que você criou para ele. Depois de exibir a resposta, você deve perguntar ao usuário se ele quer fazer outra pergunta. Caso ele responda positivamente rode o programa novamente e continue rodando desde que o usuário responda positivamente. Caso ele responda não. Finalize o programa e deixe uma mensagem agradecendo o usuário por usar seu programa.
+Seu programa deve permitir que o usuário faça uma pergunta para o programa, seu programa deve exibir na tela que está pensando
+(isso deve tomar alguns segundos) e depois o programa deve responder o usuário, dentro de 10 possibilidades que você criou para ele. 
+Depois de exibir a resposta,você deve perguntar ao usuário se ele quer fazer outra pergunta.
+Caso ele responda positivamente rode o programa novamente e continue rodando desde que o usuário responda positivamente. 
+Caso ele responda não. Finalize o programa e deixe uma mensagem agradecendo o usuário por usar seu programa.
 
 Dicas iniciais:
 Conceitos que podem ser usados:
@@ -13,8 +17,10 @@ import random
 class RespondendoPerguntas():
     def __init__(self):
         self.resp = ["Você está certo disso!", "Você é que sabe!", "Neste momento não tenho resposta para a sua pergunta", 
-                     "Você deve seguir a sua intuição!", "Eu também não gosto..", "Tenho a certeza que sim", "Sinceramente... sem comentários",
-                    "Vou fazer de conta que você, não me fez essa pergunta", "A seu tempo as coisas irão se resolver", "Tente jogar no  Euromilhões", "Tens que obedecer aos teus pais", "A melhor resposta, esta em nossos corações"]
+                     "Você deve seguir a sua intuição!", "Eu também não gosto..", "Tenho a certeza que sim",
+                     "Sinceramente... sem comentários", "Vou fazer de conta que você, não me fez essa pergunta",
+                     "A seu tempo as coisas irão se resolver", "Tente jogar no  Euromilhões", 
+                     "Tens que obedecer aos teus pais", "A melhor resposta, esta em nossos corações"]
             
     def pergunta(self):
         
